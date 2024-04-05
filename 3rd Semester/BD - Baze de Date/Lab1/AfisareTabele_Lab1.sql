1d3b24652e8506d4e0d84213bb5488b643e055a3
AriiSM <arii.stan75@gmail.com>
2024-04-05 14:41:11 +0300

    Add

---
+++ 3rd Semester/BD - Baze de Date/Lab1/AfisareTabele_Lab1.sql
@@ -0,0 +1,14 @@
+SELECT * FROM Clienti;
+SELECT * FROM Firma_transport;
+SELECT * FROM Comanda;
+SELECT * FROM Facturi_emise;
+SELECT * FROM ComandaFacturi;
+SELECT * FROM Angajati;
+SELECT * FROM Soferi;
+SELECT * FROM Masini;
+SELECT * FROM Costuri;
+SELECT * FROM Furnizori_de_piese_auto;
+SELECT * FROM MasiniFurnizori;
+SELECT * FROM Facturi_incasate;
+
+
