61daf87fe51b7b7c886b442369b3c1c02672140c
AriiSM <arii.stan75@gmail.com>
2024-09-10 14:16:21 +0300

    Remove

--- 3rd Semester/BD - Baze de Date/Lab3/CreareTabeleVersiuni.sql
+++
@@ -1,7 +0,0 @@
---Creez un simplu tabel care contine o sg coloane unde tin minte in ce versiune sunt
-CREATE TABLE _version(
-	versionNumber INT DEFAULT 0
-);
-
-
-INSERT INTO _version(versionNumber) VALUES (0);
